376f219384aa1133e4f7cd570a2385168bcb1cf0
SeongukCho <tjddnr377@naver.com>
2023-03-22 10:50:55 +0900

    실습 4-6

--- src/main/java/Ex04_06.java
+++ src/main/java/Ex04_06.java
@@ -4,6 +4,6 @@ public class Ex04_06 {
 
         System.out.printf(" AND 연산 : %s \n", (a >= 100) && (a <= 200));
         System.out.printf(" OR 연산 : %s \n", (a >= 100) || (a <= 200));
-        System.out.printf(" NOT 연산 : %s \n", !(a == 200));
+        System.out.printf(" NOT 연산 : %s \n", !(a == 100));
     }
 }
