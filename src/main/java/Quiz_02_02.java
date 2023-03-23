8238433e2b6366f25ad8f732421135d23373ae5d
SeongukCho <tjddnr377@naver.com>
2023-03-23 17:11:02 +0900

    퀴즈02_02

---
+++ src/main/java/Quiz_02_02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Quiz_02_02 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a;
+
+        System.out.printf("정수를 입력하세요 : ");
+        a = s.nextInt();
+
+        if (a % 3 == 0 & a % 5 == 0) {
+            System.out.printf("3과 5의 배수입니다.");
+        } else {
+            if (a % 3 == 0) {
+                System.out.printf("3의 배수 입니다.");
+            }
+            if (a % 5 == 0) {
+                System.out.printf("5의 배수 입니다.");
+            }
+        }
+    }
+}
