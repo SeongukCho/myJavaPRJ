440926b4b9882c3f9a84802702840b9c6674df4d
SeongukCho <tjddnr377@naver.com>
2023-03-27 12:32:45 +0900

    백준 2588

---
+++ src/main/java/baek11382.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class baek11382{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        long a,b,c;
+
+        a = s.nextLong();
+        b = s.nextLong();
+        c = s.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
