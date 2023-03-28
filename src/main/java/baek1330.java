c6c91e6ff9f8942d3854b650f7816235dab35648
SeongukCho <tjddnr377@naver.com>
2023-03-28 12:40:27 +0900

    백준 1330,9498

---
+++ src/main/java/baek1330.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class baek1330 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a,b;
+
+        a = s.nextInt();
+        b = s.nextInt();
+
+        if (a > b)
+        {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else if (a == b) {
+            System.out.println("==");
+        }
+    }
+}
