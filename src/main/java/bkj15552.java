04b8c60651ead5fd5cf8a65d63d9249b86f16a7a
SeongukCho <tjddnr377@naver.com>
2023-03-29 18:29:55 +0900

    bkj

---
+++ src/main/java/bkj15552.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class bkj15552 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+    }
+}
