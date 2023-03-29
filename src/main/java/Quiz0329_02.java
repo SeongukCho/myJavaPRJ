2a25bde2aed9a54517163c057a6e9c651d4dafc2
SeongukCho <tjddnr377@naver.com>
2023-03-29 13:20:10 +0900

    Quiz0329_02

--- src/main/java/Quiz0329_02.java
+++ src/main/java/Quiz0329_02.java
@@ -1,5 +1,12 @@
 public class Quiz0329_02 {
     public static void main(String[] args) {
-        int i,k;
+        int i, k;
+
+        for (k = 1; k < 10; k++) {
+            for (i = 2; i < 10; i++) {
+                System.out.printf("%d * %d = %d ", i, k, i * k);
+            }
+            System.out.println();
+        }
     }
 }
