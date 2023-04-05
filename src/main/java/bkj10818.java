b63fb57d5a11bff35fc865ad11337e15cd77f352
SeongukCho <tjddnr377@naver.com>
2023-04-05 14:41:47 +0900

    백준 배열

---
+++ src/main/java/bkj10818.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class bkj10818 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+        int min = arr[0], max = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.printf("%d %d", min, max);
+    }
+}
