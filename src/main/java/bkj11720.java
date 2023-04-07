ca67fa71a2a4b31f4ffbe69564cada36c8703bae
SeongukCho <tjddnr377@naver.com>
2023-04-07 15:50:14 +0900

    04/07 백준11720

---
+++ src/main/java/bkj11720.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bkj11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String st = br.readLine();
+        char ch;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            ch = st.charAt(i);
+            sum += ch - 48;
+        }
+
+        System.out.print(sum);
+    }
+}
