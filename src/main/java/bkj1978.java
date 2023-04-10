b19edce1a2600feb6d4a26cf1ad8a558176872c4
SeongukCho <tjddnr377@naver.com>
2023-04-10 16:52:37 +0900

    04/10 백준 2455

---
+++ src/main/java/bkj1978.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class bkj1978 {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
---
+++ src/main/java/bkj3003.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bkj3003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int k, q, l, b, n, p;
+
+        k = 1 - Integer.parseInt(st.nextToken());
+        q = 1 - Integer.parseInt(st.nextToken());
+        l = 2 - Integer.parseInt(st.nextToken());
+        b = 2 - Integer.parseInt(st.nextToken());
+        n = 2 - Integer.parseInt(st.nextToken());
+        p = 8 - Integer.parseInt(st.nextToken());
+
+        System.out.printf("%d %d %d %d %d %d", k, q, l, b, n, p);
+    }
+}
