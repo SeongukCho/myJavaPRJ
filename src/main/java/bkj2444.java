48377d470962307b3d389f028e4e79dbac15a06e
SeongukCho <tjddnr377@naver.com>
2023-04-11 17:44:24 +0900

    04/11 별찍기 심화

--- src/main/java/bkj2444.java
+++ src/main/java/bkj2444.java
@@ -5,11 +5,20 @@ public class bkj2444 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for (int i = 1; i <= 2 * n - 1; i++) {
-            for (int j = 2 * n - 1; j > i; j -= 2) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < i; j += 2) {
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n - 1; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2*(n-i)-1; k++) {
                 System.out.print("*");
             }
             System.out.println();
