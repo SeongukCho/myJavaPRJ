772f77c20130e686306b9bbb9f24d4a5109c0002
SeongukCho <tjddnr377@naver.com>
2023-04-12 12:35:44 +0900

    0412 백준 while문 반복

--- src/main/java/bkj1110.java
+++ src/main/java/bkj1110.java
@@ -5,7 +5,14 @@ import java.io.InputStreamReader;
 public class bkj1110 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = 10 * (n % 10) + (n / 10 + n % 10) % 10;
+        int cnt = 1;
 
-
+        while (n != m) {
+            m = 10 * (m % 10) + (m / 10 + m % 10) % 10;
+            cnt++;
+        }
+        System.out.println(cnt);
     }
 }
