5806baeb14733682e34618dfb52f5714cf48517b
SeongukCho <tjddnr377@naver.com>
2023-04-13 15:19:43 +0900

    0413 백준 2908

---
+++ src/main/java/bkj2908.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bkj2908 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int c = (a % 10) * 100 + ((a / 10) % 10) * 10 + a / 100;
+        int d = (b % 10) * 100 + ((b / 10) % 10) * 10 + b / 100;
+
+        if (c > d) {
+            System.out.println(c);
+        } else {
+            System.out.println(d);
+        }
+    }
+}
