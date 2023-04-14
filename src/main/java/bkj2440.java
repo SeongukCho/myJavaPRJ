ff4ee67c777895e4f5590ad69d22771e61454b7b
SeongukCho <tjddnr377@naver.com>
2023-04-14 13:57:58 +0900

    0414 백준 2440 별찍기3

--- src/main/java/bkj2440.java
+++ src/main/java/bkj2440.java
@@ -2,13 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class bkj11654 {
+public class bkj2440 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int n = br.readLine().charAt(0);
-
-        System.out.println(n);
-
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
