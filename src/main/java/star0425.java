d03ebf9d613e08823174104af15881bc7f94fc8a
SeongukCho <tjddnr377@naver.com>
2023-05-02 14:07:37 +0900

    0502 백준 2751

---
+++ src/main/java/star0425.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class star0425 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i=1; i <= 2 * n - 1; i++) {
+        }
+    }
+}
