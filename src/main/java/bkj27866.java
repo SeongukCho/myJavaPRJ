bde2d4436c703388819bdf60e7a767c1d1efac03
SeongukCho <tjddnr377@naver.com>
2023-05-03 12:45:01 +0900

    0503 백준 27866

---
+++ src/main/java/bkj27866.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class bkj27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int i = sc.nextInt();
+
+        System.out.println(str.substring(i-1,i));
+    }
+}
