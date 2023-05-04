f6de61400d563196180ea6288e815fa0ada86e1f
SeongukCho <tjddnr377@naver.com>
2023-05-04 15:02:03 +0900

    0504 백준 2675

---
+++ src/main/java/bkj2675.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class bkj2675 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int r = sc.nextInt();
+            String str = sc.next();
+
+            int length = str.length();
+
+            String[] strarr = str.split("",length);
+            for (int j = 0; j < length; j++) {
+                for (int k = 0; k < r; k++) {
+                    System.out.print(strarr[j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
