44f4f4ec99952d0cc91294496ae861f9214316ed
SeongukCho <tjddnr377@naver.com>
2023-05-11 09:52:52 +0900

    0510 백준 1100

---
+++ src/main/java/bkj1427.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class bkj1427 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String st = br.readLine();
+        String[] strr = st.split("");
+        Arrays.sort(strr, Comparator.reverseOrder());
+        for (String s : strr) {
+            System.out.print(s);
+        }
+    }
+}
